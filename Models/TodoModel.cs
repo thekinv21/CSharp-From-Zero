f118bc12acb83aefeaa40b74641551faec9a1ab6
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-30 23:58:17 +0300

    fix: [master][Vadim]: TodoModel.cs düzeltildi

--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -1,14 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using TodoApp.Enum;
 
 namespace TodoApp.Models;
 
-public class TodoModel:BaseModel
+public class TodoModel : BaseModel
 {
     public int Id { get; set; }
 
-    [Required] [StringLength(100)] public string? Title { get; set; }
+    [StringLength(100)] public string? Title { get; set; }
 
-    [Required] [StringLength(400)] public string? Description { get; set; }
+    [StringLength(400)] public string? Description { get; set; }
 
-    public bool IsComplete { get; set; }
+    public Priority Priority { get; set; }
+
+    public bool IsComplete { get; set; } = false;
+
+    public int Order { get; set; }
 }

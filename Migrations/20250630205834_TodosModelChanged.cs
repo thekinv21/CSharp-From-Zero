4e454147ae2ca1f83c78003cb41ae1109fc16d8f
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-30 23:59:26 +0300

    feat: [master][Vadim]: Migration snapshotları eklendi

---
+++ Migrations/20250630205834_TodosModelChanged.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class TodosModelChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "character varying(400)",
+                maxLength: 400,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(400)",
+                oldMaxLength: 400);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Todos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Todos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Todos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "character varying(400)",
+                maxLength: 400,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(400)",
+                oldMaxLength: 400,
+                oldNullable: true);
+        }
+    }
+}
